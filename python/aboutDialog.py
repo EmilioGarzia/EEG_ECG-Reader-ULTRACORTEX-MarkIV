2efcd67790e113901ad18a298489bc3852c83124
Emilio Garzia <emilio.garzia001@studenti.uniparthenope.it>
2023-03-20 09:47:43 +0100

    Emilio Garzia
    fixed bug

--- python/aboutDialog.py
+++ python/aboutDialog.py
@@ -1,7 +1,10 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import platform
+
+separator = "\\" if platform.system() == "Windows" else "/"
 
 class AboutDialog(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi("..\\GUI\\aboutDialog.ui", self)
+        uic.loadUi("..{0}GUI{0}aboutDialog.ui".format(separator), self)
