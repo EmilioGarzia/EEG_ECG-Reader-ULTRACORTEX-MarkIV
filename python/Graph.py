1727a4692de6fcff01a0acfe92df38da3257b141
Emilio Garzia <emilio.garzia001@studenti.uniparthenope.it>
2023-03-20 13:56:37 +0100

    Emilio Garzia
    Update GroupChannels

--- python/Graph.py
+++ python/Graph.py
@@ -1,8 +1,10 @@
 import pyqtgraph as pg
 
 class Graph(pg.PlotWidget):
-    def __init__(self):
+    def __init__(self, xrange, yrange):
         super().__init__()
+        self.setXRange(xrange[0], xrange[1])
+        self.setYRange(yrange[0], yrange[1])
         self.plots = []
 
         self.showGrid(x=True, y=True)
@@ -12,11 +14,11 @@ class Graph(pg.PlotWidget):
         self.lightTheme()
         
 
-    def lightTheme(self):
-        self.setBackground("white")
-        
-    def darkTheme(self):
-        self.setBackground("black")
+    def lightTheme(self): self.setBackground("white")
+    def darkTheme(self): self.setBackground("black")
+
+    def hidePlot(self, index): self.removeItem(self.plots[index-1])
+    def showPlot(self, index): self.addItem(self.plots[index-1])
 
     def addPlot(self, color):
         pen = pg.mkPen(color=color)
@@ -27,3 +29,4 @@ class Graph(pg.PlotWidget):
     def refresh(self, data):
         for i in range(len(self.plots)):
             self.plots[i].setData(data[i].x, data[i].y)
+    
--- python/main.py
+++ python/main.py
@@ -10,14 +10,13 @@ import aboutDialog
 import fileDialog
 
 #global var
-board = None
-waveWidget = None
-fftWidget = None
 separator = "\\" if platform.system() == "Windows" else "/"  #file system separator
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, board):
         super().__init__()
+        self.board = board
+
         #GUI loader
         uic.loadUi("..{0}GUI{0}gui.ui".format(separator), self)
         #File browser object
@@ -27,28 +26,28 @@ class MainWindow(QMainWindow):
         self.aboutWindow = aboutDialog.AboutDialog()
 
         #Wave Plot Instruction
-        global waveWidget
-        waveWidget = Graph()
-        waveWidget.setXRange(-board.num_points, 0)
-        waveWidget.setYRange(-20000, 20000)
-        init_series(waveWidget)
-        self.addWavePlot(waveWidget)
+        self.waveWidget = Graph((-board.num_points, 0), (-20000, 20000))
+        self.addGraph(self.waveWidget, self.waveContainer)
         
         # FFT Plot Instruction
-        global fftWidget
-        fftWidget = Graph()
-        fftWidget.setXRange(0, 60)
-        fftWidget.setYRange(0, 10000)
-        init_series(fftWidget)
-        self.addFTTPlot(fftWidget)
+        self.fftWidget = Graph((0, 60), (0, 10000))
+        self.addGraph(self.fftWidget, self.fftContainer)
         
         #start GUI in dark mode
         self.darkMode()
 
-    def start(self):
-        global board
-        board.connect(self.fileManager.getPath())
-        self.startLoop(update, 1000//board.sampling_rate)
+
+    def start(self, ):
+        self.board.connect(self.fileManager.getPath())
+        self.startLoop(self.update, 1000//self.board.sampling_rate)
+
+    #Functions that update plot data
+    def update(self):
+        wave, fft = self.board.read_data()
+        if wave is not None:
+            self.waveWidget.refresh(wave)
+        if fft is not None:
+            self.fftWidget.refresh(fft)
 
     #some methods of MainWindow
     def showFileManager(self): 
@@ -56,27 +55,66 @@ class MainWindow(QMainWindow):
         self.openedFileLabel.setText(self.fileManager.getFilename())
 
     def showAbout(self): self.aboutWindow.show()
-    def addWavePlot(self, widget): self.waveContainer.addWidget(widget)
-    def addFTTPlot(self, widget): self.fftContainer.addWidget(widget)
+
+    def addGraph(self, graph, container):
+        for i in range(len(self.board.exg_channels)):
+            color = self.board.get_channel_color(i+1)
+            graph.addPlot(color)
+        container.addWidget(graph)
     
     #Methods for theme
     def lightMode(self):
         with open("..{0}css{0}styleLight.css".format(separator), "r") as css:
-            global waveWidget
-            global fftWidget
             myCSS = css.read()
             self.setStyleSheet(myCSS)
-            waveWidget.lightTheme()
-            fftWidget.lightTheme()
+            self.waveWidget.lightTheme()
+            self.fftWidget.lightTheme()
         
     def darkMode(self):
         with open("..{0}css{0}styleDark.css".format(separator), "r") as css:
-            global waveWidget
-            global fftWidget
             myCSS = css.read()
             self.setStyleSheet(myCSS)
-            waveWidget.darkTheme()
-            fftWidget.darkTheme()
+            self.waveWidget.darkTheme()
+            self.fftWidget.darkTheme()
+
+    def toggledChannel(self, state):
+        x = int(self.sender().text())
+        if state == 2:
+            self.waveWidget.showPlot(x)
+            self.fftWidget.showPlot(x)
+        else: 
+            self.waveWidget.hidePlot(x)
+            self.fftWidget.hidePlot(x)
+
+    def checkBoxSetter(self, status):
+        self.CH1check.setChecked(status)
+        self.CH2check.setChecked(status)
+        self.CH3check.setChecked(status)
+        self.CH4check.setChecked(status)
+        self.CH5check.setChecked(status)
+        self.CH6check.setChecked(status)
+        self.CH7check.setChecked(status)
+        self.CH8check.setChecked(status)
+        self.CH9check.setChecked(status)
+        self.CH10check.setChecked(status)
+        self.CH11check.setChecked(status)
+        self.CH12check.setChecked(status)
+        self.CH13check.setChecked(status)
+        self.CH14check.setChecked(status)
+        self.CH15check.setChecked(status)
+        self.CH16check.setChecked(status)
+
+    def selectAllChannel(self, state):
+        if state == 2:
+            for x in self.board.exg_channels:
+                self.waveWidget.showPlot(x)
+                self.fftWidget.showPlot(x)
+                self.checkBoxSetter(True)
+        else:
+            for x in self.board.exg_channels:
+                self.waveWidget.hidePlot(x)
+                self.fftWidget.hidePlot(x)
+                self.checkBoxSetter(False)
 
     #Methods for Show/Hide plot
     def show_hide_wave(self, state):
@@ -100,10 +138,10 @@ class MainWindow(QMainWindow):
 
     #Methods for loop
     def startLoop(self, loop, delay):
-            self.timer = QtCore.QTimer()
-            self.timer.setInterval(delay)
-            self.timer.timeout.connect(loop)
-            self.timer.start()
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(delay)
+        self.timer.timeout.connect(loop)
+        self.timer.start()
 
     def stopLoop(self): self.timer.stop()
 
@@ -111,28 +149,14 @@ class MainWindow(QMainWindow):
 # ****************************************** - - - Main block - - - *****************************************#
 def main():
     #Connect GUI to Cyton board
-    global board
-    board = CytonDaisyBoard("/dev/ttuUSB0")
+    board = CytonDaisyBoard("/dev/ttyUSB0")
     
     #main application
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = MainWindow(board)
     window.show()
     sys.exit(app.exec_())
 
-
-#Functions that update plot data
-def update():
-    wave, fft = board.read_data()
-    if wave is not None:
-        waveWidget.refresh(wave)
-    if fft is not None:
-        fftWidget.refresh(fft)
-
-def init_series(graph):
-    for i in range(len(board.exg_channels)):
-        color = board.get_channel_color(i+1)
-        graph.addPlot(color)
 
 #Start Process
 if __name__ == "__main__":
